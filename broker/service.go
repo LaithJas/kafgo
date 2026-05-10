1d7e6ecf4792eadf476dfac4f63ac27af4c6dc4e
Laith Abou Saleh <laithas.007@gmail.com>
2026-05-10 15:06:05 -0700

    added start function and broker methods to service.go

--- broker/service.go
+++ broker/service.go
@@ -3,7 +3,13 @@
 package broker
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
 	"github.com/laithjas/kafgo/proto"
+	"google.golang.org/grpc"
 )
 
 type broker struct {
@@ -15,6 +21,46 @@ func NewBroker() *broker {
 	return &broker{
 		cache: newCache(),
 	}
+}
+
+// starts a grpc server that will serve using a TCP conneection
+func (b *broker) Start(ctx context.Context) error {
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		return fmt.Errorf("tcp server listener failed")
+	}
+	s := grpc.NewServer()
+	proto.RegisterBrokerServiceServer(s, b)
+
+	ch := make(chan error, 1)
+	go func() {
+		err2 := s.Serve(ln)
+		if err2 != nil {
+			ch <- err2
+		}
+	}()
+	select {
+	case <-ctx.Done():
+		s.GracefulStop()
+		return nil
+	case v := <-ch:
+		return fmt.Errorf("connection failed: %v", v)
+	}
+}
+
+func (b *broker) CreateTopic(ctx context.Context, request *proto.CreateTopicRequest) (*proto.CreateTopicResponse, error) {
+	response := proto.CreateTopicResponse{}
+	err := b.cache.create(request.Topic)
+	if err != nil {
+		response.Success = false
+		response.StatusCode = err.Error()
+		return &response, err
+	}
+	response.Success = true
+	response.StatusCode = "OK"
+	response.Topic = request.Topic
+
+	return &response, nil
 }
 
 // first open a service that cosumers can connect to send their data
